97debaa3eec96333be95cd8251363151509b7899
aytugan242 <147434124+aytugan242@users.noreply.github.com>
2024-08-05 14:29:24 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,30 @@
+import requests
+
+# response = requests.post('http://127.0.0.1:8000/v1/advertisement/',
+#                          json={
+#                              "title": "title",
+#                              'description': "description 1",
+#                              'author': "author 1",
+#                              'price': 50.0
+#                          })
+#
+# print(response.json())
+# print(response.status_code)
+
+# response = requests.patch('http://127.0.0.1:8000/v1/advertisement/4/',
+#                           json={"title": "title new"}
+#                           )
+# print(response.json())
+# print(response.status_code)
+
+# response = requests.delete('http://127.0.0.1:8000/v1/advertisement/4/',)
+# print(response.json())
+# print(response.status_code)
+
+# response = requests.get('http://127.0.0.1:8000/v1/advertisement/1/',)
+# print(response.json())
+# print(response.status_code)
+#
+# response = requests.get('http://127.0.0.1:8000/v1/advertisement?title=title',)
+# print(response.status_code)
+# print(response.text)
